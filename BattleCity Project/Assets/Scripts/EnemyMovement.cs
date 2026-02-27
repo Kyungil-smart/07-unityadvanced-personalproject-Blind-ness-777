0c31cdc1a228f877fe745e012500a1b5b60f3d7e
Blind-ness-777 <wlsgod10@gmail.com>
2026-02-27 10:25:26 +0900

    [feat] Create EnemyMovement

---
+++ BattleCity Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 3f;
+
+    private Vector3 direction;
+    private Vector3 lookDir;
+
+    private void Awake()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+
+    public void Tick(Vector3 moveDir)
+    {
+        direction = moveDir;
+
+        if (direction != Vector3.zero)
+        {
+            lookDir = direction;
+            rb.MoveRotation(Quaternion.LookRotation(lookDir));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction == Vector3.zero) return;
+        
+        Vector3 currentPos = rb.position;
+        float distance = speed * Time.fixedDeltaTime;
+
+        Vector3 nextPos = currentPos + direction * distance;
+        rb.MovePosition(nextPos);
+    }
+}
