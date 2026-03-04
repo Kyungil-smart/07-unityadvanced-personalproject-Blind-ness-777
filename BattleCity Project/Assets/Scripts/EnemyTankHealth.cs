fea852ce3d6d73ff27b98246f6aa99bc74fdd716
Blind-ness-777 <wlsgod10@gmail.com>
2026-03-04 21:33:20 +0900

    feat: Add EnemyTankHealth, PlayerTankLife, GameUIManager

---
+++ BattleCity Project/Assets/Scripts/EnemyTankHealth.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyTankHealth : MonoBehaviour, IProjectileHittable
+{
+    [SerializeField] private TankData tankData;
+    private int currentHp;
+
+    private void Awake()
+    {
+        currentHp = tankData != null ? tankData.maxHp : 1;
+    }
+
+    public void OnProjectileHit(Projectile projectile, RaycastHit hit)
+    {
+        TakeHit(1);
+    }
+
+    public void TakeHit(int damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        GameManager.Instance?.AddScore(tankData != null ? tankData.scoreValue : 0);
+        ItemManager.Instance?.TrySpawnItem();
+        FindObjectOfType<SpawnManager>()?.OnEnemyDied();
+        gameObject.SetActive(false);
+    }
+}
